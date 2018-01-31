9d6bd3237c9cc9d500318b96d1bcaecfa9ffa6cb
JWood <1987boxior@gmail.com>
2018-02-01 00:19:46 +0200

    add component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app">
+        <header className="app__header">
+            <div className="header">
+                <div className="header__title">
+                    $<output className="header__sum">167</output>&nbsp;still needed for this project
+                </div>
+            </div>
+
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <article className="app__content.content"></article>
       </div>
     );
   }
