b148ab94c0af9388a8a72fbc4e8363d7248cbdca
serhii.l@itua.com.ua <b13579GL>
2018-02-07 19:26:10 +0200

    fixed error in rendering

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import Navigation from './Components/Navigation/Navigation';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <Navigation/>,
     document.getElementById("root")
 );
-// registerServiceWorker();
+registerServiceWorker();
 
